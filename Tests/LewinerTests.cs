c1dab1492782afb407a3dc4578e6cf4187928cdb
Frank A. Krueger <fak@praeclarum.org>
2022-02-02 16:24:13 -0800

    Add Tests

---
+++ Tests/LewinerTests.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace Tests;
+
+public class LewinerTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
