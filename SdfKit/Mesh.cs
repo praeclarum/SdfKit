f922089f3005ebb945aa8cc3bfa45df28a43881d
Frank A. Krueger <fak@praeclarum.org>
2022-02-03 11:13:18 -0800

    Make Mesh a volume

--- SdfKit/Mesh.cs
+++ SdfKit/Mesh.cs
@@ -5,7 +5,7 @@ using System.IO;
 /// <summary>
 /// A triangle mesh with normals.
 /// </summary>
-public class Mesh
+public class Mesh : IVolume
 {
     public Vector3[] Vertices { get; }
     public Vector3[] Normals { get; }
