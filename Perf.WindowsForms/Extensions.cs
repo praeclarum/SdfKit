4ae7d88e68263c6ea9ce5bf04a18c9b23f8d9791
mrange <marten_range@hotmail.com>
2022-02-06 10:34:13 +0100

    Added Windows.Forms SDF Perf project

---
+++ Perf.WindowsForms/Extensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Perf.WindowsForms
+{
+  public static class Extensions
+  {
+    public static void RunInUIThread(this SynchronizationContext sync, Action a)
+    {
+      sync.Post(d => a(), null);
+    }
+  }
+}
