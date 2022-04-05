add33cc91a0987265b2624a16232b46efad27745
Frank A. Krueger <fak@praeclarum.org>
2022-04-05 11:38:31 -0700

    Fix ICP when rotating

--- SdfKit/IterativeClosestPoint.cs
+++ SdfKit/IterativeClosestPoint.cs
@@ -16,18 +16,25 @@ public class IterativeClosestPoint
 
     public int MaxIterations { get; } = 100;
 
-    // public int Corresponses { get; } = 1000;
+    // public int MaxCorresponses { get; } = 1000;
 
-    public float Tolerance { get; } = 0.01f;
+    public float GoodCorrespondenceDistance { get; set; } = 0.01f;
 
-    const float convergedTranslation = 0.001f;
-    const float convergedRotation = 0.001f;
+    public float ConvergedMaximumTranslation { get; set; } = 1.0e-4f;
+
+    public float ConvergedMaximumRotation { get; set; } = 1.0e-5f;
 
     public IterativeClosestPoint (ReadOnlySpan<Vector3> staticPoints)
     {
         staticTree = new KdTree (staticPoints);
     }
 
+    /// <summary>
+    /// Register points with the static points used to construct this instance.
+    /// The returned transform converts from the static coordinate system to the
+    /// input points coordinate system. The inverse of this transform is used to
+    /// move the input points into the static coordinate system.
+    /// </summary>
     public Matrix4x4 RegisterPoints (Span<Vector3> points)
     {
         var totalTransform = Matrix4x4.Identity;
@@ -44,7 +51,7 @@ public class IterativeClosestPoint
             var delta = transform;
             var drot = MathF.Abs (1.0f-delta.M11) + MathF.Abs (1.0f-delta.M22) + MathF.Abs (1.0f-delta.M33);
             var dtrans = delta.Translation.Length ();
-            converged = dtrans <= convergedTranslation && drot <= convergedRotation;
+            converged = dtrans <= ConvergedMaximumTranslation && drot <= ConvergedMaximumRotation;
 
             lastTransform = transform;
             totalTransform = totalTransform * transform;
@@ -55,122 +62,131 @@ public class IterativeClosestPoint
     Matrix4x4 GetIterTransform (Span<Vector3> points, Matrix<float> p, Matrix<float> q)
     {
         var npoints = points.Length;
-
-        // Find the corresponding points in the static tree
         var corA = vpool.Rent (npoints);
-        var cor = corA.AsSpan (0, npoints);
         var distA = fpool.Rent (npoints);
-        var dist = distA.AsSpan (0, npoints);
-        var distMean = 0.0f;
-        for (var i = 0; i < npoints; i++) {
-            cor[i] = staticTree.Search (points[i], out dist[i]);
-            distMean += dist[i];
-        }
-        distMean /= npoints;
 
-        // Find distance statistics for filtering
-        var distStd = 0.0f;
-        for (var i = 0; i < npoints; i++) {
-            var d = dist[i] - distMean;
-            distStd += d * d;
-        }
-        distStd = MathF.Sqrt (distStd / npoints);
-        float distMax;
-        if (distMean < Tolerance) {
-            distMax = distMean + 3.0f * distStd;
-        }
-        else if (distMean < 3.0f * Tolerance) {
-            distMax = distMean + 2.0f * distStd;
-        }
-        else if (distMean < 6.0f * Tolerance) {
-            distMax = distMean + distStd;
-        }
-        else {
-            // "We have chosen in our implementation the valley after the maximal peak as the value of distMax"
-            distMax = distMean + 0.5f + distStd;
-        }
-
-        // Filter out points that are too far away and find the mean points
-        // p points are the dynamic points
-        // q points are the corresponding static points
-        var nfilter = 0;
-        var pmean = Vector3.Zero;
-        var qmean = Vector3.Zero;
-        for (var i = 0; i < npoints; i++) {
-            if (dist[i] <= distMax) {
-                p[nfilter, 0] = points[i].X;
-                p[nfilter, 1] = points[i].Y;
-                p[nfilter, 2] = points[i].Z;
-                q[nfilter, 0] = cor[i].X;
-                q[nfilter, 1] = cor[i].Y;
-                q[nfilter, 2] = cor[i].Z;
-                nfilter++;
-                pmean += points[i];
-                qmean += cor[i];
+        try {
+            // Find the corresponding points in the static tree
+            var cor = corA.AsSpan (0, npoints);
+            var dist = distA.AsSpan (0, npoints);
+            var distMean = 0.0f;
+            for (var i = 0; i < npoints; i++) {
+                cor[i] = staticTree.Search (points[i], out dist[i]);
+                distMean += dist[i];
             }
+            distMean /= npoints;
+
+            // Find distance statistics for filtering
+            var distStd = 0.0f;
+            for (var i = 0; i < npoints; i++) {
+                var d = dist[i] - distMean;
+                distStd += d * d;
+            }
+            distStd = MathF.Sqrt (distStd / npoints);
+            float distMax;
+            if (distMean < GoodCorrespondenceDistance) {
+                distMax = distMean + 3.0f * distStd;
+            }
+            else if (distMean < 3.0f * GoodCorrespondenceDistance) {
+                distMax = distMean + 2.0f * distStd;
+            }
+            else if (distMean < 6.0f * GoodCorrespondenceDistance) {
+                distMax = distMean + distStd;
+            }
+            else {
+                // "We have chosen in our implementation the valley after the maximal peak as the value of distMax"
+                distMax = distMean + 0.5f + distStd;
+            }
+
+            // Filter out points that are too far away and find the mean points
+            // p points are the dynamic points
+            // q points are the corresponding static points
+            var nfilter = 0;
+            var pmean = Vector3.Zero;
+            var qmean = Vector3.Zero;
+            for (var i = 0; i < npoints; i++) {
+                if (dist[i] <= distMax) {
+                    p[nfilter, 0] = points[i].X;
+                    p[nfilter, 1] = points[i].Y;
+                    p[nfilter, 2] = points[i].Z;
+                    q[nfilter, 0] = cor[i].X;
+                    q[nfilter, 1] = cor[i].Y;
+                    q[nfilter, 2] = cor[i].Z;
+                    nfilter++;
+                    pmean += points[i];
+                    qmean += cor[i];
+                }
+            }
+            pmean /= nfilter;
+            qmean /= nfilter;
+
+            // Center p and q using their means
+            for (var i = 0; i < nfilter; i++) {
+                p[i, 0] -= pmean.X;
+                p[i, 1] -= pmean.Y;
+                p[i, 2] -= pmean.Z;
+                q[i, 0] -= qmean.X;
+                q[i, 1] -= qmean.Y;
+                q[i, 2] -= qmean.Z;
+            }
+
+            // Compute the cross correlation matrix
+            var c = Matrix<float>.Build.Dense (3, 3);
+            for (var i = 0; i < nfilter; i++) {
+                var px = p[i, 0];
+                var py = p[i, 1];
+                var pz = p[i, 2];
+                var qx = q[i, 0];
+                var qy = q[i, 1];
+                var qz = q[i, 2];
+                c[0, 0] += px * qx;
+                c[0, 1] += px * qy;
+                c[0, 2] += px * qz;
+                c[1, 0] += py * qx;
+                c[1, 1] += py * qy;
+                c[1, 2] += py * qz;
+                c[2, 0] += pz * qx;
+                c[2, 1] += pz * qy;
+                c[2, 2] += pz * qz;
+            }
+
+            // Use Svd to compute the rotation matrix
+            var svd = c.Svd ();
+            var u = svd.U;
+            var ut = u.Transpose ();
+            var vt = svd.VT;
+            var v = vt.Transpose ();
+            var detSign = MathF.Sign ((v * ut).Determinant ());
+            var sd = Matrix<float>.Build.Diagonal (new float[] { 1, 1, detSign });
+            var r = v * sd * ut;
+            var rMatrix = new Matrix4x4 (
+                r[0, 0], r[0, 1], r[0, 2], 0,
+                r[1, 0], r[1, 1], r[1, 2], 0,
+                r[2, 0], r[2, 1], r[2, 2], 0,
+                0, 0, 0, 1
+            );
+            Matrix4x4.Invert (rMatrix, out var invRMatrix);
+
+            // Calculate the translation vector
+            var pRotatedMean = Vector3.Transform (pmean, invRMatrix);
+            var translation = pRotatedMean - qmean;
+
+            // Calculate the transformation matrix
+            var transformMatrix = rMatrix * Matrix4x4.CreateTranslation (translation);
+
+            // Apply the transformation to the points
+            Matrix4x4.Invert (transformMatrix, out var invTransformMatrix);
+            for (var i = 0; i < npoints; i++) {
+                points[i] = Vector3.Transform (points[i], invTransformMatrix);
+            }
+
+            // Return the transformation matrix
+            return transformMatrix;
         }
-        pmean /= nfilter;
-        qmean /= nfilter;
-
-        // Center p and q using their means
-        for (var i = 0; i < nfilter; i++) {
-            p[i, 0] -= pmean.X;
-            p[i, 1] -= pmean.Y;
-            p[i, 2] -= pmean.Z;
-            q[i, 0] -= qmean.X;
-            q[i, 1] -= qmean.Y;
-            q[i, 2] -= qmean.Z;
+        finally {
+            vpool.Return (corA);
+            fpool.Return (distA);
         }
-
-        // Compute the cross correlation matrix
-        var c = Matrix<float>.Build.Dense (3, 3);
-        for (var i = 0; i < nfilter; i++) {
-            var px = p[i, 0];
-            var py = p[i, 1];
-            var pz = p[i, 2];
-            var qx = q[i, 0];
-            var qy = q[i, 1];
-            var qz = q[i, 2];
-            c[0, 0] += px * qx;
-            c[0, 1] += px * qy;
-            c[0, 2] += px * qz;
-            c[1, 0] += py * qx;
-            c[1, 1] += py * qy;
-            c[1, 2] += py * qz;
-            c[2, 0] += pz * qx;
-            c[2, 1] += pz * qy;
-            c[2, 2] += pz * qz;
-        }
-
-        // Use Svd to compute the rotation matrix
-        var svd = c.Svd ();
-        var u = svd.U;
-        var ut = u.Transpose ();
-        var vt = svd.VT;
-        var v = vt.Transpose ();
-        var detSign = MathF.Sign ((v * ut).Determinant ());
-        var sd = Matrix<float>.Build.Diagonal (new float[] { 1, 1, detSign });
-        var r = v * sd * ut;
-        var rMatrix = new Matrix4x4 (
-            r[0, 0], r[0, 1], r[0, 2], 0,
-            r[1, 0], r[1, 1], r[1, 2], 0,
-            r[2, 0], r[2, 1], r[2, 2], 0,
-            0, 0, 0, 1
-        );
-
-        // Calculate the translation vector
-        var translation = qmean - Vector3.Transform (pmean, rMatrix);
-
-        // Calculate the transformation matrix
-        var transformMatrix = rMatrix * Matrix4x4.CreateTranslation (translation);
-
-        // Apply the transformation to the points
-        for (var i = 0; i < npoints; i++) {
-            points[i] = Vector3.Transform (points[i], transformMatrix);
-        }
-
-        // Return the transformation matrix
-        return transformMatrix;
     }
 }
 
--- Tests/IterativeClosestPointTests.cs
+++ Tests/IterativeClosestPointTests.cs
@@ -7,23 +7,60 @@ public class IterativeClosestPointTest
         new Vector3 (0, 1, 0),
         new Vector3 (1, 0, 0),
     };
-    static readonly Vector3[] threePointsOffsetX = new Vector3[] {
-        new Vector3 (0+0.1f, 0, 1),
-        new Vector3 (0+0.1f, 1, 0),
-        new Vector3 (1+0.1f, 0, 0),
-    };
+
+    static Vector3[] TransformPoints (Vector3[] points, Matrix4x4 transform)
+    {
+        var npoints = points.Length;
+        var result = new Vector3[npoints];
+        for (var i = 0; i < npoints; i++) {
+            result [i] = Vector3.Transform (points [i], transform);
+        }
+        return result;
+    }
+
+    public void ThreePointsTest (Matrix4x4 expectedTransform)
+    {
+        var cp = new IterativeClosestPoint (threePoints);
+        var transformedPoints = TransformPoints (threePoints, expectedTransform);
+        var transform = cp.RegisterPoints (transformedPoints);
+        var translation = transform.Translation;
+        var expectedTranslation = expectedTransform.Translation;
+        Assert.AreEqual (expectedTranslation.X, translation.X, 1.0e-4f);
+        Assert.AreEqual (expectedTranslation.Y, translation.Y, 1.0e-4f);
+        Assert.AreEqual (expectedTranslation.Z, translation.Z, 1.0e-4f);
+        Assert.AreEqual (expectedTransform.M11, transform.M11, 1.0e-6f);
+        Assert.AreEqual (expectedTransform.M22, transform.M22, 1.0e-6f);
+        Assert.AreEqual (expectedTransform.M33, transform.M33, 1.0e-6f);
+        for (var i = 0; i < threePoints.Length; i++) {
+            var p = threePoints [i];
+            var q = transformedPoints [i];
+            Assert.AreEqual (p.X, q.X, 1.0e-4f);
+            Assert.AreEqual (p.Y, q.Y, 1.0e-4f);
+            Assert.AreEqual (p.Z, q.Z, 1.0e-4f);
+        }
+    }
 
     [Test]
     public void ThreePointsOffsetX ()
     {
-        var cp = new IterativeClosestPoint (threePoints);
-        var transform = cp.RegisterPoints (threePointsOffsetX);
-        var translation = transform.Translation;
-        Assert.AreEqual (-0.1f, translation.X, 1.0e-4f);
-        Assert.AreEqual (0.0f, translation.Y, 1.0e-4f);
-        Assert.AreEqual (0.0f, translation.Z, 1.0e-4f);
-        Assert.AreEqual (1.0f, transform.M11, 1.0e-4f);
-        Assert.AreEqual (1.0f, transform.M22, 1.0e-4f);
-        Assert.AreEqual (1.0f, transform.M33, 1.0e-4f);
+        ThreePointsTest (Matrix4x4.CreateTranslation (0.1f, 0, 0));
+    }
+
+    [Test]
+    public void ThreePointsOffsetXYZ ()
+    {
+        ThreePointsTest (Matrix4x4.CreateTranslation (0.1f, -0.2f, -0.3f));
+    }
+
+    [Test]
+    public void ThreePointsRotateY ()
+    {
+        ThreePointsTest (Matrix4x4.CreateRotationY (1.0f * MathF.PI / 180.0f));
+    }
+
+    [Test]
+    public void ThreePointsRotateXOffsetY ()
+    {
+        ThreePointsTest (Matrix4x4.CreateRotationX (1.0f * MathF.PI / 180.0f) * Matrix4x4.CreateTranslation (0, 0.1f, 0));
     }
 }
